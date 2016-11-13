590077218b90d9dbd5885d14d56b6c286416f50f
ericgold <egold05@gmail.com>
2016-11-12 17:33:25 -0800

    organization and gameBoard

--- treasure-chest.js
+++ treasure-chest.js
@@ -1,8 +1,58 @@
 (function(exports) {
+
+// ******* DATA OBJECT ******************************
+	var DATA = {
+		student : "Eric Gold",
+		date : "11-12-16",
+		organization : "Hack Oregon",
+		department : "Hack University",
+		course : "JavaScript Foundations",
+		instructor : "Merritt Lawrenson"
+	};
+
+// ******* METHODS ********************************** 
+//min
+//isEven
+//countChar
+//assignGrade
+//line (make into function with argument?)
+//string (make into function with argument?)
+//board
+	exports.gameBoard = function(horizontal, vertical) {
+		var sizeX = horizontal;
+		var sizeY = vertical;
+		var board = "";
+
+		for (var y = 0; y < sizeY; y++) {
+			for (var x = 0; x < sizeX; x++) {
+				if ((x+y) % 2 == 0)
+					board += " ";
+				else
+					board += "#";
+		}
+		board += "\n";
+	}
+
 	
+//isPrime
+//betterRange
+//reverseArray 
+//madArguments
+//compare
+//readNotRead
+//Person
+//repeatify
+//Vector
+//month-names
 
-}(typeof window === 'undefined' || window === null ? global.treasureChest = {} : window.treasureChest = {});
+// ******* PROTOTYPES ********************************
 
 
+// ******* SUB-MODULES *******************************
+
+
+}(typeof window === 'undefined' || window === null ? global.treasureChest = {} : window.treasureChest = {})
+
+console.log(treasureChest.gameBoard(8,8));
 
 
